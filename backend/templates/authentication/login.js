70ff154b80f710d40f3eb2392d7a4f29008d7f58
dmiv <dmitriy40647274@gmail.com>
2022-06-29 12:34:44 +0300

    save token in session storage

--- backend/templates/authentication/login.js
+++ backend/templates/authentication/login.js
@@ -1,3 +1,4 @@
+// myStorage = window.localStorage;
 var app = new Vue({
     el: '#app',
     data: () => ({
@@ -7,15 +8,21 @@ var app = new Vue({
     }),
     methods: {
         login () {
-            console.log(this.username, this.password);
+            // console.log(this.username, this.password);
             axios.post('http://127.0.0.1:8000/api/users/login/', {
                 user: {
-                    username: 'dmiv',
-                    password: 123456
+                    username: this.username,
+                    password: this.password,
                 }
               })
               .then(function (response) {
-                console.log(response);
+                // console.log(response.data.user.token);
+                localStorage.setItem('token', response.data.user.token);
+                sessionStorage.setItem('token', response.data.user.token);
+                // myStorage.setItem('dmiv', 'dmiv');
+                window.location = 'fight.html';
+                // this.cookie.set('name', 'dmiv');
+                // window.location = 'file:///home/dmiv/PycharmProjects/hitman/backend/templates/fight/fight.html';
               })
               .catch(function (error) {
                 console.log(error);
@@ -23,15 +30,3 @@ var app = new Vue({
         }
     }
 })
-// axios.post('http://127.0.0.1:8000/api/users/login/', {
-//     user: {
-//         username: 'dmiv',
-//         password: 123456
-//     }
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   });
