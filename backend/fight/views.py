7935de1b64f88107019dbeb812c316c76723740a
dmiv <dmitriy40647274@gmail.com>
2022-07-02 11:41:14 +0300

    debug save scores

--- backend/fight/views.py
+++ backend/fight/views.py
@@ -13,10 +13,12 @@ def score(user_one, user_two):
     one_defend = user_one.defend
     two_attack = user_two.attack
     two_defend = user_two.defend
-    scores = {0: 3, 1: 2, 3: 1, 4: 1, 5: 1, 6: 1}
+    scores = {'0': 3, '1': 2, '2': 1, '3': 1, '4': 1, '5': 1}
+    # print(one_attack, two_attack, one_defend, two_defend)
     one_score = [scores[i] if i not in two_defend else 0 for i in one_attack]
     two_score = [scores[i] if i not in one_defend else 0 for i in two_attack]
-    return one_score, two_score
+    # print('scores: ', one_score, two_score)
+    return sum(one_score), sum(two_score)
 
 class StatisticView(viewsets.ModelViewSet):
     serializer_class = StatisticSerializer
@@ -28,12 +30,15 @@ class StatisticView(viewsets.ModelViewSet):
         if len(players) < 2:
             return 'please wait'
         one_score, two_score = score(players[0], players[1])
-        players[0].score, players[1].score = one_score, two_score
+        # print('scores', one_score, two_score)
+        # players[0].score, players[1].score = one_score, two_score
         for i in players:
             i.finished = True
             i.save()
-        num = Statistic.objects.last().num_round + 1 if len(Statistic.objects.all()) > 0 else 1
-        return Statistic.objects.create(num_round=num, first_player=players[0], second_player=players[1])
+        # print('scr2', players[0].__dict__, players[1].__dict__)
+        num = Statistic.objects.last().num_round + 1 if len(Statistic.objects.all()) > 0 else 1 # set num round
+        return Statistic.objects.create(num_round=num, first_player=players[0], second_player=players[1],
+                                        first_player_score=one_score, second_player_score=two_score)
 
 
 class FightView(APIView):
