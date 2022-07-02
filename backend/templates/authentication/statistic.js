0e4f7a05dc6780730624fa403a38ab48158f3425
dmiv <dmitriy40647274@gmail.com>
2022-07-02 09:43:57 +0300

    change show statistic

--- backend/templates/authentication/statistic.js
+++ backend/templates/authentication/statistic.js
@@ -1,12 +1,29 @@
-axios.get('http://127.0.0.1:8000/fight/statistic/', {
+function part_body (data) {
+      var len = data.length
+      var part_body = {0: 'head', 1: 'body', 2: 'left hand', 3: 'right hand', 4: 'left foot', 5: 'right foot'};
+      for (let i=0; i < len; i++) {
+          for (let j=0; j<3; j++) {
+           data[i].first_player.attack[j] = part_body[data[i].first_player.attack[j]];
+           data[i].first_player.defend[j] = part_body[data[i].first_player.defend[j]];
+           data[i].second_player.attack[j] = part_body[data[i].second_player.attack[j]];
+           data[i].second_player.defend[j] = part_body[data[i].second_player.defend[j]];
+          }
+          data[i].first_player.attack = data[i].first_player.attack.join(', ');
+          data[i].first_player.defend = data[i].first_player.defend.join(', ');
+          data[i].second_player.attack = data[i].second_player.attack.join(', ');
+          data[i].second_player.defend = data[i].second_player.defend.join(', ');
+      };
+}
 
+axios.get('http://127.0.0.1:8000/fight/statistic/', {
   })
   .then(function (response) {
-      console.log(response);
+      var data = response.data;
+      part_body(data);
       var app = new Vue({
           el: '#app',
           data: {
-              rounds: response.data
+              rounds: data
           }
       })
   })
