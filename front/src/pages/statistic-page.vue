82198c632183984c6f51d14bbef403bb8c29cc67
dmiv <dmitriy40647274@gmail.com>
2022-09-08 22:52:04 +0300

    change count "__" in statistic

--- front/src/pages/statistic-page.vue
+++ front/src/pages/statistic-page.vue
@@ -11,17 +11,17 @@
         <h3>Player {{ round.first_player.user }} VS Player {{ round.second_player.user }}</h3>
         <p>Player {{ round.first_player.user }} attack the: {{
             round.first_player.attack.join(', ')
-          }}____________________________________________________________________________________________________________
+          }}_______________________________________________________________________________________________________
           Player {{ round.second_player.user }} attack the: {{ round.second_player.attack.join(', ') }}
         </p>
         <p>Player {{ round.first_player.user }} defend the: {{
             round.first_player.defend.join(', ')
-          }}____________________________________________________________________________________________________________
+          }}_______________________________________________________________________________________________________
           Player {{ round.second_player.user }} defend the: {{ round.second_player.defend.join(', ') }}
         </p>
         <h3> {{ round.winner }} </h3>
         <p>Player {{ round.first_player.user }}: {{ round.first_player_score }}
-          score_____________________________________________________________________________________________________________________________
+          score________________________________________________________________________________________________________________________
           Player {{ round.second_player.user }}: {{ round.second_player_score }} score</p>
       </li>
     </ol>
