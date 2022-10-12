10856358ce2e92d475818a578d6cb451bd78b040
dmiv <dmitriy40647274@gmail.com>
2022-10-12 20:20:41 +0300

    add form for post data

--- djangoProject2/APP/views.py
+++ djangoProject2/APP/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import Technics
 
 def technice_view(request):
-    # print(request.method)
+    if request.method == 'POST':
+        # print('post', request.POST)
+        data = request.POST
+        name = data.get('name')
+        age = data.get('age')
+        print(age)
+        print(name)
+    if request.method == 'GET':
+        print('get', request.GET)
     data = Technics.objects.all()
     return render(request, 'APP/index.html', {'data': data})
